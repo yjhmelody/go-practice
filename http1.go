fea6ec4acaffd69d2b47e5186dd88bbbc1c1045a
yjhmelody <465402634@qq.com>
2017-08-20 17:24:45 +0800

    update

--- http1.go
+++ http1.go
@@ -23,7 +23,23 @@ func (d dollars) String() string {
 type database map[string]dollars
 
 func (db database) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	for k, v := range db {
-		fmt.Fprintf(w, "%s: %s\n", k, v)
+	switch req.URL.Path {
+	case "/list":
+		for k, v := range db {
+			fmt.Fprintf(w, "%s: %s\n", k, v)
+		}
+	case "/price":
+		item := req.URL.Query().Get("item")
+		price, ok := db[item]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, "no such item: %q\n", item)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", price)
+
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such page: %s\n", req.URL)
 	}
 }
