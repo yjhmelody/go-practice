e5e7f360885d5dee165aaea17687221aeccf3d19
yjhmelody <465402634@qq.com>
2017-09-05 23:48:43 +0800

    update

--- sync.go
+++ sync.go
@@ -6,16 +6,16 @@ import (
 	"time"
 )
 
-func main(){
+func main() {
 	var mutex sync.Mutex
 	mutex.Lock()
-	for i := 1; i <= 3; i++{
+	for i := 1; i <= 3; i++ {
 		go func(i int) {
 			fmt.Printf("Lock the lock. g%d\n", i)
 			mutex.Lock()
 			fmt.Printf("The lock is locked. g%d\n", i)
 		}(i)
-		
+
 		time.Sleep(time.Second)
 		fmt.Println("Unlock the lock.")
 		mutex.Unlock()
---
+++ web1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	// 解析参数
+	r.ParseForm()
+	//输出到服务器端的打印信息
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("value:", strings.Join(v, ""))
+	}
+	//这个写入到w的是输出到客户端的
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
